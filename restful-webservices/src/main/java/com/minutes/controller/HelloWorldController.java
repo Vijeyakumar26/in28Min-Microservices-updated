3f94c294aff00df8e93d94f466025a27f8684576
Vijeyakumar R <vijeyakumar.26@gmail.com>
2024-04-11 23:52:27 +0530

    code changes for @PathVariable

--- restful-webservices/src/main/java/com/minutes/controller/HelloWorldController.java
+++ restful-webservices/src/main/java/com/minutes/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.minutes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.minutes.beans.HelloWorldBean;
@@ -16,5 +17,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-bean")
 	public HelloWorldBean getHelloBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-bean/{name}")
+	public HelloWorldBean getHelloBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World ,%s", name));
 	}
 }
