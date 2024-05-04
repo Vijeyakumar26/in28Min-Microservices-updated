07828f54a531ba59fef8f13993616c7a1873f758
Vijeyakumar R <vijeyakumar.26@gmail.com>
2024-05-04 23:56:23 +0530

    code changes for Content negtiation and Internationalization

--- restful-webservices/src/main/java/com/minutes/controller/HelloWorldController.java
+++ restful-webservices/src/main/java/com/minutes/controller/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.minutes.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +26,18 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-bean/{name}")
 	public HelloWorldBean getHelloBeanPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World ,%s", name));
+	}
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController( MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	@GetMapping(path = "/hello-internationalized")
+	public String getHelloInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);		
 	}
 }
