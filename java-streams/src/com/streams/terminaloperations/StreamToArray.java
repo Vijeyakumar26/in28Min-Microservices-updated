bb2c8d10eff07956c69bed92ce446240260f7562
Vijeyakumar R <vijeyakumar.26@gmail.com>
2024-06-23 02:56:26 +0530

    stream terminal operations

--- java-streams/src/com/streams/terminaloperations/StreamToArray.java
+++ java-streams/src/com/streams/terminaloperations/StreamToArray.java
@@ -1,10 +1,15 @@
 package com.streams.terminaloperations;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamToArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> numbers = Arrays.asList(2,1,4,7,10); 
+		Integer[] array = numbers.stream()
+		 .filter((Integer s) -> s>3)
+		 .toArray((int size) -> new Integer[size]);
 	}
 
 }
